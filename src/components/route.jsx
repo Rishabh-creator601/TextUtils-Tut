28e4cc1c543997bfdaefcbe95a4ce8cee23b0dca
Rishabh <rishuvisionworld@gmail.com>
2024-10-05 20:39:39 +0530

    small2

--- src/components/route.jsx
+++ src/components/route.jsx
@@ -7,12 +7,12 @@ import Contacts from "./Contacts";
 
 let router =  createBrowserRouter(
     createRoutesFromElements(
-        <Route exact path="/" element={<Layout />}>
+        <Route exact path="/textutils-react" element={<Layout />}>
             <Route path="" element={<MainForm />}/>
             <Route path="/contacts" element={<Contacts />} />
 
         </Route>
-    )
+    ),{basename:"/textutils-react"}
 )
 
 
