e0def062539c69534f0b1cc312cb97e4ed74b807
Rishabh <rishuvisionworld@gmail.com>
2024-10-05 20:32:08 +0530

    small

--- src/components/route.jsx
+++ src/components/route.jsx
@@ -1,31 +1,19 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter , createRoutesFromElements,Route} from "react-router-dom";
 import MainForm from "./MainForm";
 import Layout from "../Layout";
 import Contacts from "./Contacts";
-import { Children } from "react";
 
 
-let router =  createBrowserRouter([
-    {
-        path:"/",
-        element:<Layout />,
 
-    
-   children: [
+let router =  createBrowserRouter(
+    createRoutesFromElements(
+        <Route exact path="/" element={<Layout />}>
+            <Route path="" element={<MainForm />}/>
+            <Route path="/contacts" element={<Contacts />} />
 
-    {
-        path:"",
-        element:<MainForm />
-    },
-    {
-        path:"/contacts",
-        element:<Contacts />
-    }
-
-   ]
-
-}
-])
+        </Route>
+    )
+)
 
 
 export default router;
